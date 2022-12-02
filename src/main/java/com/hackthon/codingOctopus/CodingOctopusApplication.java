8ba10e8523aec13bb12c0826c47e8deeda4393aa
yongbin <rhdydqls1024@naver.com>
2022-12-02 23:10:31 +0900

    :seedling: :: intial commit

---
+++ src/main/java/com/hackthon/codingOctopus/CodingOctopusApplication.java
@@ -0,0 +1,13 @@
+package com.hackthon.codingOctopus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingOctopusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingOctopusApplication.class, args);
+	}
+
+}
